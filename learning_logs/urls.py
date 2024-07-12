ca5c2a1d4cec22e958e93e4e3bdf31def465811c
Propulson <aeroflotmili@gmail.com>
2024-07-12 15:14:31 +1200

    refactor

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     # Page with all topics
     path('topics/', views.topic, name='topics'),
+    # Page with info about theme
+    path('topics/<int:topic_id/', views.topic, name='topic'),
 ]
+
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -9,9 +9,10 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 
-def topic(request):
+def topic(request, topic_id):
     """Show list themes"""
-    topics = Topic.objects.order_by('date_added')
-    context = {'topics': topics}
-    return render(request, 'learning_log/topics.html', context)
+    topics = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
 
